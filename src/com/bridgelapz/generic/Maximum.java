2f88b6e71dc110d377c69ec7ec3de0e64b2b2f61
Shlok Kumar Mishra <shlok7869@gmail.com>
2023-04-04 15:24:26 +0530

    More Than Three Parameter

--- src/com/bridgelapz/generic/Maximum.java
+++ src/com/bridgelapz/generic/Maximum.java
@@ -2,22 +2,22 @@ package com.bridgelapz.generic;
 
 public class Maximum {
 
-	public static <T extends Comparable<T>> T maximum(T x, T y, T z) {
-
-		T max = x;
-		if (y.compareTo(max) > 0) {
-			max = y;
+	public static <T extends Comparable<T>> void Max(T value1 ,T value2 ,T value3,T value4 ){
+		T maximum=value1;
+		
+		if(value2.compareTo(maximum)>0) {
+			maximum=value2;
 		}
-		if (z.compareTo(max) > 0) {
-
-			max = z;
+		if(value3.compareTo(maximum)>0) {
+			maximum=value3;
 		}
-
-		return max;
+		if(value4.compareTo(maximum)>0) {
+			maximum=value4;
+		}
+		System.out.println("Maximum value:"+" "+maximum);
 	}
-
+	
 	public static void main(String args[]) {
-		System.out.println("Find The Maximum Float Value - "+" " + maximum("Apple","Peach","Banana"));
+		Max(12,22,34,555);
 	}
 }
-//Apple Peach Banana
