a97322198be39cd71a84ebdabc23c8ae167afba2
Shlok Kumar Mishra <shlok7869@gmail.com>
2023-04-04 21:04:36 +0530

    Max

--- src/com/bridgelapz/generic/Maximum.java
+++ src/com/bridgelapz/generic/Maximum.java
@@ -1,23 +1,43 @@
 package com.bridgelapz.generic;
 
-public class Maximum {
+public class Maximum < T extends Comparable <T>>{
 
-	public static <T extends Comparable<T>> void Max(T value1 ,T value2 ,T value3,T value4 ){
-		T maximum=value1;
-		
-		if(value2.compareTo(maximum)>0) {
-			maximum=value2;
-		}
-		if(value3.compareTo(maximum)>0) {
-			maximum=value3;
-		}
-		if(value4.compareTo(maximum)>0) {
-			maximum=value4;
-		}
-		System.out.println("Maximum value:"+" "+maximum);
-	}
+	T x;
+	T y;
+	T z;
 	
-	public static void main(String args[]) {
-		Max(12,22,34,555);
+	
+ public Maximum(T x, T y, T z) {
+		super();
+		this.x = x;
+		this.y = y;
+		this.z = z;
 	}
+ 
+ public  T maximum() {
+	 return Maximum.maximum(x, y, z);
+ }
+
+public static <T extends Comparable>T  maximum(T x ,T y,T z){
+	T max=x;
+	if(y.compareTo(max)>0) {
+		max=y;
+	}
+		if(z.compareTo(max)>0) {
+			max=z;
+	}
+	printMax(x,y,z,max);
+	return max;
+}
+
+public static <T> void printMax(T x,T y,T z,T max){
+	System.out.println(x , y , z ,max);
+}
+public static void main(String args[]) {
+	Integer xint=13, yint=14,zint=34;
+	Float xflt=12.0f, yflt=14.0f,zflt=16.0f;
+	
+	Maximum.maximum(xint, yint, zint);
+	Maximum.maximum(xflt, yflt, zflt);
+}
 }
